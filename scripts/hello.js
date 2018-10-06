4dbde9c161d0d0f5c035a27ad41acd5bc538458d
nayochev <nakahazu10@gmail.com>
2018-10-06 06:51:24 +0000

    おみくじ機能実装

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,7 +1,20 @@
 'use strict';
+
+//あいさつ
 module.exports = (robot) => {
 	robot.hear(/hello>/i, (msg) => {
 		const username = msg.message.user.name;
 		msg.send('Hello, ' + username);
 	});
 };
+
+//おみくじ
+const lots = ['大吉', '吉', '中吉', '末吉', '凶'];
+module.exports = (omikuji) => {
+	omikuji.hear(/lot>/i, (msg) => {
+		const lot = lots[Math.floor(Math.random() * lots.length)];
+		msg.send(lot + '、nakahazu10');
+	});
+};
+
+
